90a883b921b36d7db6991b0fa34efd3d52cc1169
secure-analyst-1223 <z.chium@gmail.com>
2020-03-09 15:26:43 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,10 +1,11 @@
+#array variables
 row = [" ", " ", " "," ", " ", " ", " ", " "]
 row[0-8]
 
 row[1] = "X"
 
 #board design
-puts "\n  #{row[0]}  | #{row[1]}  | #{row[2]}  |   
+puts "\n  #{row[0]}  | #{row[1]}  | #{row[2]}  |
 \n-----------------\n  #{row[3]}  | #{row[4]}  | #{row[5]}  |
 \n-----------------\n  #{row[6]}  | #{row[7]}  | #{row[8]}  |   "
 #first,second and third rows
