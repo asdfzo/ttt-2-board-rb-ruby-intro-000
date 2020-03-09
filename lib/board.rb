a3f9c516de5dc2782adc0e3b2557c300064ac955
secure-analyst-1223 <z.chium@gmail.com>
2020-03-09 15:37:18 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,23 +1,23 @@
 # array variables
 board = [" ", " ", " "," ", " ", " ", " ", " "]
 # Position 1: Left
-row[0]
+board[0]
 # Position 2: Middle
-row[1]
+board[1]
 # Position 3: Right
-row[2]
+board[2]
 # Position 4: Middle Left
-row[3]
+board[3]
 # Position 5: Middle Center
-row[4]
+board[4]
 # Position 6: Middle Right
-row[5]
+board[5]
 # Position 7: Bottom Left
-row[6]
+board[6]
 # Position 8: Bottom Middle
-row[7]
+board[7]
 # Position 9: Bottom Right
-row[8]
+board[8]
 
 # design
 puts design = "\n  #{row[0]}  | #{row[1]}  | #{row[2]}  |
