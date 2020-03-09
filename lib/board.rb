09d906bfc5e16c6378cb6997cf7c9f870c937b4b
secure-analyst-1223 <z.chium@gmail.com>
2020-03-09 15:12:57 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,12 +1,3 @@
-# Define the variable board below.
-row = [" ", " ", " "]
-# Postions 1: Left
-row[0]
 
-# Postition 2: Middle
-row[1]
-
-# Postitions 3: Right
-row[2]
 
 puts "\n   |   |   |   \n----------------\n    |   |   |   \n----------------\n    |   |   |   "
