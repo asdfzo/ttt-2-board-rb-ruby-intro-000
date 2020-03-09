da8cb4f310d545c92c766d59e981693ee91caeb9
secure-analyst-1223 <z.chium@gmail.com>
2020-03-09 15:30:28 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,8 +1,23 @@
 #array variables
 row = [" ", " ", " "," ", " ", " ", " ", " "]
-row[0-8]
-
-row[1] = "X"
+#Position 1: Left 
+row[0]
+#Position 2: Middle
+row[1]
+#Position 3: Right
+row[2]
+#Position 4: Middle Left
+row[3]
+#Position 5: Middle Center
+row[4]
+#Position 6: Middle Right
+row[5]
+#Position 7: Bottom Left
+row[6]
+#Position 8: Bottom Middle
+row[7]
+#Position 9: Bottom Right
+row[8]
 
 #board design
 puts "\n  #{row[0]}  | #{row[1]}  | #{row[2]}  |
