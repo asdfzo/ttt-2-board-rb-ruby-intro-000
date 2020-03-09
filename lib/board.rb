6e3f25dc4eb58069875afd10ac5380bad5b463d2
secure-analyst-1223 <z.chium@gmail.com>
2020-03-09 15:25:44 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,7 +1,10 @@
-#board design
-puts "\n  #{row[0]}  | #{row[]}  |   |   \n-----------------\n    |   |   |   \n-----------------\n    |   |   |   "
-#first,second and third rows
 row = [" ", " ", " "," ", " ", " ", " ", " "]
 row[0-8]
 
 row[1] = "X"
+
+#board design
+puts "\n  #{row[0]}  | #{row[1]}  | #{row[2]}  |   
+\n-----------------\n  #{row[3]}  | #{row[4]}  | #{row[5]}  |
+\n-----------------\n  #{row[6]}  | #{row[7]}  | #{row[8]}  |   "
+#first,second and third rows
