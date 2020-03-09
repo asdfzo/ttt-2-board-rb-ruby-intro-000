8c15063b8aa8df1003d7a6b7cbdc6355e4329d92
secure-analyst-1223 <z.chium@gmail.com>
2020-03-09 14:58:49 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,15 @@
 # Define the variable board below.
+row = [" ", " ", " "]
+# Postions 1: Left
+row[0]
+
+# Postition 2: Middle
+row[1]
+
+# Postitions 3: Right
+row[2]
+
+#move X to the Position 2, Middle
+row[1] = "X"
+
+puts row[]
