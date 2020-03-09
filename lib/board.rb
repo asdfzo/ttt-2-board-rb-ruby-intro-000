8fadf26320e0a699f66b72e80a80a2af59ee37a0
secure-analyst-1223 <z.chium@gmail.com>
2020-03-09 15:19:53 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,7 @@
+#board design
 puts "\n    |   |   |   \n-----------------\n    |   |   |   \n-----------------\n    |   |   |   "
+#first,second and third rows
+row = [" ", " ", " "]
+row[0]
+row[1]
+row[3]
