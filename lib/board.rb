2fac0584aa42627f3fab491dca90dd29195ff074
secure-analyst-1223 <z.chium@gmail.com>
2020-03-09 15:31:33 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,22 +1,22 @@
-#array variables
+# array variables
 row = [" ", " ", " "," ", " ", " ", " ", " "]
-#Position 1: Left
+# Position 1: Left
 row[0]
-#Position 2: Middle
+# Position 2: Middle
 row[1]
-#Position 3: Right
+# Position 3: Right
 row[2]
-#Position 4: Middle Left
+# Position 4: Middle Left
 row[3]
-#Position 5: Middle Center
+# Position 5: Middle Center
 row[4]
-#Position 6: Middle Right
+# Position 6: Middle Right
 row[5]
-#Position 7: Bottom Left
+# Position 7: Bottom Left
 row[6]
-#Position 8: Bottom Middle
+# Position 8: Bottom Middle
 row[7]
-#Position 9: Bottom Right
+# Position 9: Bottom Right
 row[8]
 
 # design
